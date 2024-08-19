6e989a479fc6287c0ad16e5dc630191ef1d772c8
AndressaSales <andressaccs2001@gmail.com>
2024-08-19 16:38:07 -0300

    navbar atualizada

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,73 +1,83 @@
-import Link from "next/link"
 import Image from "next/image"
+import Link from "next/link"
 import { useState } from "react"
 
 export default function Navbar(){
 
-    const [menu, setMenu] = useState<boolean>(false)
+    const [btnMenu, setBtnMenu] = useState<boolean>(false)
 
-    const toogleMenu = () =>{
-        setMenu(!menu)
+    const menu = () => {
+        setBtnMenu(!btnMenu)
     }
 
     return(
         <main>
-            <div className="navbar justify-between flex py-7">
-                <div className="flex items-center justify-center  flex-shrink-0 pl-6">
+            <nav className="justify-between flex py-7 navbar">
+                
+                <div className="items-center flex justify-center pl-3">
                     <Link href={'/'}>
                         <Image 
                             src="/assets/logo.png"
                             alt="logo"
-                            width={80}
-                            height={70}
+                            width={80} height={70}
                             className="img"
                         />
                     </Link>
                 </div>
 
-                <div className="block lg:hidden">
-                    <button onClick={toogleMenu} className="btn">
-                        {
-                            menu ? (
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-8">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                                </svg>
+                <div className="lg:hidden block">
+                    <button
+                        className="btn" 
+                        onClick={menu}
+                    >
+                        {btnMenu ? 
+                            (
+                                <Image 
+                                    src="/assets/close.svg"
+                                    alt="close"
+                                    width={30} height={30}
+                                />
                             ) : (
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-8">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                                </svg>
-
-                            )
-                        }
+                                <Image 
+                                    src="/assets/menu.svg"
+                                    alt="menu"
+                                    width={30} height={30}
+                                />
+                            )}
                     </button>
                 </div>
 
-                <div className="link block sm:hidden lg:items-center lg:flex grow lg:w-auto">
-                    <div className="text-center flex lg:grow">
-                        <Link href={'/Produtos'} className="ho pt-4 font text-2xl lg:ml-80 lg:mr-32">PRODUTOS</Link>
-                        <Link href={'/Passatempo'} className="ho lg:pt-4 text-2xl lg:mr-32 font">PASSATEMPO</Link>
-                        <Link href={'/Contatos'} className="ho lg:pt-4 text-2xl font">CONTATOS</Link>
-                    </div>
-
-                </div>
-            </div>
-            {
-                menu && (
-                    <div className="block flex-grow lg:hidden text-center">
-                       <div className="grid grid-cols-1 text-center flex">
-                            <Link href={'/Produtos'} className="ho font sm:p-4 text-2xl">
+                <div className="lg:items-center lg:flex flex-grow w-full lg:w-auto block sm:hidden">
+                    <div className="lg:flex-grow text-center">
+                            <Link className="text-2xl lg:mr-32 pt-4 ho" href={'/Produtos'}>
                                 PRODUTOS
                             </Link>
-                            <Link href={'/Passatempo'} className="ho sm:pb-4 font text-2xl">
+                            <Link className="lg:pt-4 text-2xl lg:mr-32 ho" href={'/Passatempo'}>
                                 PASSATEMPO
                             </Link>
-                            <Link href={'/Contatos'} className="text-2xl font ho">
+                            <Link className="lg:pt-4 text-2xl ho" href={'/Contatos'}>
                                 CONTATOS
                             </Link>
-                       </div>
+                    </div>
+                </div>
+            </nav>
+            {
+                btnMenu && (
+                    <div className="flex-grow lg:hidden text-center block">
+                        <div className="grid grid-cols-1 text-center flex">
+                            <Link className="sm:p-4 text-2xl ho" href={'/Produtos'}>
+                                PRODUTOS
+                            </Link>
+                            <Link className="text-2xl sm:pb-4" href={'/Passatempo'}>
+                                PASSATEMPO
+                            </Link>
+                            <Link className="text-2xl ho" href={'/Contatos'}>
+                                CONTATOS
+                            </Link>
+                        </div>
                     </div>
                 )
-            }
+                }
         </main>
     )
 }
